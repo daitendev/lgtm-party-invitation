853298b19ef82ee2416cf6f81aac79e946925df2
daitendev <18205123+daitendev@users.noreply.github.com>
2022-02-15 00:00:39 +0900

    fix: content script injction

--- package/js/background.js
+++ package/js/background.js
@@ -1,12 +1,22 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(async () => {
   chrome.contextMenus.create({
     type: 'normal',
     id: 'lgtm-party-invitation-menu',
     title: 'Invite to LGTM.party',
     contexts: ['image']
   });
+  for (const cs of chrome.runtime.getManifest().content_scripts) {
+    for (const tab of await chrome.tabs.query({ url: cs.matches })) {
+      if (!tab.url.startsWith('https://chrome.google.com/webstore')) {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: cs.js,
+        });
+      }
+    }
+  }
 });
 
-chrome.contextMenus.onClicked.addListener((_, tab) => {
-  chrome.tabs.sendMessage(tab.id, 'getClickedEl');
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+  chrome.tabs.sendMessage(tab.id, { cmd: 'captureImageSrc', src: item.srcUrl });
 });
--- package/js/content.js
+++ package/js/content.js
@@ -1,10 +1,6 @@
-document.addEventListener('contextmenu', (event) => {
-  clickedEl = event.target;
-}, true);
-
 chrome.runtime.onMessage.addListener((request) => {
-  const src = clickedEl.src
-  if (request === 'getClickedEl' && src) {
+  const { cmd, src } = request;
+  if (cmd === 'captureImageSrc' && src) {
     const createPage = window.open('https://lgtm.party/extension/invite')
     window.addEventListener('message', (event) => {
       if (event.data.loaded === true) {
