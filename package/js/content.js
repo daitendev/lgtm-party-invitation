edd350d243586c31ecec903e5437891644b237ad
daitendev <18205123+daitendev@users.noreply.github.com>
2022-02-18 21:59:30 +0900

    fix: window.open url

--- package/js/content.js
+++ package/js/content.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((request) => {
   const { cmd, src } = request;
   if (cmd === 'captureImageSrc' && src) {
-    const createPage = window.open('https://lgtm.party/extension/invite')
+    const createPage = window.open('https://lgtm.party/create#invite', '_blank')
     window.addEventListener('message', (event) => {
       if (event.data.loaded === true) {
         createPage.postMessage({ src }, 'https://lgtm.party');
